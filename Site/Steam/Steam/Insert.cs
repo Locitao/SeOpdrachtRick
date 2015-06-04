121c975a8b7a93767039fb52c9c23e38517eff72
Rick van Duijnhoven <rick.vanduijnhoven@student.fontys.nl>
2015-06-04 10:12:22 +0200

    Insert class finished

--- Site/Steam/Steam/Insert.cs
+++ Site/Steam/Steam/Insert.cs
@@ -10,5 +10,59 @@ namespace Steam
     /// </summary>
     public class Insert
     {
+        Connection conn = new Connection();
+
+        public string Insert_Account(string name, string password, string email, string birthdate)
+        {
+            try
+            {
+                string query =
+                    "INSERT INTO USER_ACCOUNT (acc_ID, name_user, passw, email_address, birth_date) VALUES " +
+                    "(auto_inc_acc.nextval, '" + name + "', '" + password + "', '" + email + "', '" + birthdate + "');";
+                conn.Execute(query);
+                const string succes = "Account created!";
+                return succes;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string Insert_Review(string description, int number, int accId, int productId)
+        {
+            try
+            {
+                string query = "INSERT INTO REVIEW (review_ID, rev_description, rating, acc_ID, product_ID) VALUES " +
+                           "(auto_inc_rev.nextval, '" + description + "', '" + number + "', '" + accId + "', '" +
+                           productId + "');";
+                conn.Execute(query);
+                const string succes = "Review created.";
+                return succes;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            
+        }
+
+        public string Insert_Message(int accId, int accIdTwo, string text, string date)
+        {
+            try
+            {
+                string query = "INSERT INTO POST (post_ID, acc_ID, acc_ID_two, post_text, post_date) VALUES " +
+                                           "(auto_inc_pst.nextval, '" + accId + "', '" + accIdTwo + "', '" + text + "', '" + date +
+                                           "');";
+                conn.Execute(query);
+                const string succes = "Message send.";
+                return succes;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            
+        }
     }
 }
---
+++ Site/Steam/Steam/Library.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Library.aspx.cs" Inherits="Steam.Library" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Library</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
