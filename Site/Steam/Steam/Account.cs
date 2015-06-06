2decc4fdc809c773582ca3f72b66e6a42a54e903
Rick van Duijnhoven <rick.vanduijnhoven@student.fontys.nl>
2015-06-06 14:39:28 +0200

    Account/Administration class added

---
+++ Site/Steam/Steam/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Steam
+{
+    public class Account
+    {
+        public int Id { get { return Id; } set { Id = value; } }
+        public string Name { get { return Name; } set { Name = value; } }
+        public string Password { get { return Password; } set { Password = value; } }
+        public string Email { get { return Email; } set { Email = value; } }
+        public DateTime Birthdate { get { return Birthdate; } set { Birthdate = value; } }
+        public int Balance { get { return Balance; } set { Balance = value; } }
+
+        public Account(int id, string name, string password, string email, DateTime birthdate, int balance)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            Email = email;
+            Birthdate = birthdate;
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            string data = Convert.ToString(Id) + ", " + Name + ", " + Email + ", " + Convert.ToString(Birthdate) + ".";
+            return data;
+        }
+    }
+}
---
+++ Site/Steam/Steam/Administration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Steam
+{
+    public class Administration
+    {
+        private Account account;
+        private Insert insert;
+        private Select select;
+        private Update update;
+
+        private void Update_Account(string email, string password)
+        {
+            int id = 0;
+            string name = "tempname";
+            string pw = "temppw";
+            string mail = "tempmail";
+            DateTime birthdate = new DateTime(1990, 10, 10);
+            int balance = 0;
+
+            var data = select.Select_Account(email, password);
+
+            foreach (Dictionary<string, object> row in data)
+            {
+                id = Convert.ToInt32(row["acc_ID"]);
+                name = Convert.ToString(row["name_user"]);
+                pw = Convert.ToString(row["passw"]);
+                mail = Convert.ToString(row["email_address"]);
+                birthdate = Convert.ToDateTime(row["birth_date"]);
+                balance = Convert.ToInt32(row["steam_balance"]);
+            }
+
+            account = new Account(id, name, pw, mail, birthdate, balance);
+
+        }
+
+        public string Get_Account_Data()
+        {
+            string data = account.ToString();
+            return data;
+        }
+    }
+}
